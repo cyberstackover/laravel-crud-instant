0fd3d95ae601b4cdbd2d5630f7dd959b298da694
herwin@windowslive.com <herwin@windowslive.com>
2018-03-22 12:02:22 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Herwin - CRUD</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .martop-sm {margin-top: 15px;}
+        .martop-lg {margin-top: 70px;}
+    </style>
+</head>
+<body>
+    <div class="container martop-lg">
+        <div class="panel panel-default">
+            <div class="panel-body">@yield('content')</div>
+        </div>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
